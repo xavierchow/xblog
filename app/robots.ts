7c99a3f651f5ca0caa664f243d549f1971533bea
Xavier Zhou <xiayezhou@gmail.com>
2025-02-10 17:55:32 +0800

    fix: avoid font being crawled

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/*.woff$', '/*.woff2$', '/*.ttf$', '/*.eot$', '/*.otf$'],
     },
     sitemap: `${host}/sitemap.xml`,
   };
